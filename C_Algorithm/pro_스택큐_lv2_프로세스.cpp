a16f26a20d3951b8098e7fabfa86afddbfd68d3a
Termuni <139777794+Termuni@users.noreply.github.com>
2024-06-13 16:10:03 +0900

    Process 문제 추가 코멘터리

--- C_Algorithm/pro_스택큐_lv2_프로세스.cpp
+++ C_Algorithm/pro_스택큐_lv2_프로세스.cpp
@@ -16,6 +16,11 @@ https://school.programmers.co.kr/learn/courses/30/lessons/42587
 몇 번째로 실행되는지 알고싶은 프로세스의 위치를 알려주는 location이 매개변수로 주어질 때, 
 해당 프로세스가 몇 번째로 실행되는지 return 하도록 solution 함수를 작성해주세요.
 */
+/*
+만약 queue에서 max를 찾는 함수를 알고있다면, 앞의 큐를 뽑아서 맨 뒤로 보내는 방식으로 한방에 찾는 방식이 있음
+*max_element(begin, end)를 통해 가능
+*/
+#include <queue>
 #include <string>
 #include <algorithm>
 #include <vector>
@@ -23,9 +28,13 @@ https://school.programmers.co.kr/learn/courses/30/lessons/42587
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
-    //queue의 맨 앞 확인 -> idx
-    //나머지 중 높은거 확인 -> 하나씩 확인하기
-    //제일 높은 숫자를 찾고 
+    //받은 것 front~end 확인, 프로세스 우선순위 1~9 개수 확인
+    //각 우선순위에 대해 해당 우선 순위의 개수만큼 searching, 반납
+    //필요 자료형 : int array [10] -> 1~9 갯수 저장
+    //필요 정보 : int nowIdx(priorities의 시작부터 끝까지 현재 어느 Idx인지 확인하기 위함)
+    //제일 프로세스 큰 숫자가 뭐인지 확인하는 processMax
+    //while processMax != 0 -> processMax는 9~1까지 내려올 예정
+    //nowIdx에서 제일 
     int answer = 0;
     vector<int> temp;
     vector<int> sorted = priorities;
